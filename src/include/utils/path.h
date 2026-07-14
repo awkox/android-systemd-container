d7174eefec73a713977aa2c0ee3c413dadb4748b
awkoo <133107732+awkox@users.noreply.github.com>
2026-07-14 19:07:16 +0000

    .

--- src/include/utils/path.h
+++ src/include/utils/path.h
@@ -9,7 +9,6 @@ inline const fs::path cgroup_dir   = fs::path("/sys/fs/cgroup");
 inline const fs::path mount_dir    = fs::path("/mnt/asc");
 inline const fs::path project_cgroup_dir = cgroup_dir / "asc";
 inline const fs::path runtime_dir  = tmp_dir / "asc";
-inline const fs::path config_dir   = runtime_dir / "config";
 inline const fs::path lock_dir     = runtime_dir / "lock";
 
 #endif
--- src/utils/workspace.cpp
+++ src/utils/workspace.cpp
@@ -7,6 +7,5 @@ int ensure_runtime(void) {
 
   create_directories_with_permission(runtime_dir);
   create_directories_with_permission(lock_dir);      // 修复获取锁直接失败的致命错误
-  create_directories_with_permission(config_dir);    // 规范化预创建
   return 0;
 }
