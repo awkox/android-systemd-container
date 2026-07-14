ab5d330f7a658e549e0000b77c438bca0d9eb281
awkoo <133107732+awkox@users.noreply.github.com>
2026-07-14 14:06:17 +0000

    .

--- src/oci/cgroup.cpp
+++ src/oci/cgroup.cpp
@@ -67,8 +67,8 @@ static void rmdir_cgroup_tree(const fs::path& path) {
   if (access(kill_path.c_str(), W_OK) == 0) {
     const int kfd = open(kill_path.c_str(), O_WRONLY | O_CLOEXEC);
     if (kfd >= 0) {
-      close(kfd);
       if (write(kfd, "1", 1) < 0) {}
+      close(kfd);
     }
   }
 
