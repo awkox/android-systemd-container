260f4b00f44a3cf4a523dc459b45013cb4075427
awkoo <133107732+awkox@users.noreply.github.com>
2026-07-15 05:37:07 +0000

    .

--- src/utils/system.cpp
+++ src/utils/system.cpp
@@ -11,10 +11,6 @@ int get_kernel_version(int *major, int *minor) {
   return 0;
 }
 
-void oom_protect(void) {
-  FILE *f = fopen("/proc/self/oom_score_adj", "w");
-  if (f) {
-    fprintf(f, "-1000\n");
-    fclose(f);
-  }
+void oom_protect() {
+    std::ofstream("/proc/self/oom_score_adj") << "-1000\n";
 }
