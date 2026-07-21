f9207e9d1635d0aa97584128ce11a1132651357a
awkoo <133107732+awkox@users.noreply.github.com>
2026-07-21 13:59:50 +0000

    .

--- src/include/utils/string.h
+++ src/include/utils/string.h
@@ -1,9 +1,8 @@
 #ifndef ASC_UTILS_STRING_H
 #define ASC_UTILS_STRING_H
 
-#include <filesystem>
+#include <string_view>
 #include <string>
-#include <sys/types.h>
 
 int reject_container_name(std::string_view name);
 std::string format_privileged_mask(const int mask);
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include <cstdio>
-#include <string>
 #include <string_view>
 
 #include "utils/string.h"
